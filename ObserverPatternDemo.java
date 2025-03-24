becbf6d7c386efae2af5355a7628379ff9a50c3d
Ahmed ayar Khan <okhoori3399@gmail.com>
2025-03-24 03:51:50 -0700

    chane

--- ObserverPatternDemo.java
+++ ObserverPatternDemo.java
@@ -5,11 +5,11 @@ public class ObserverPatternDemo {
        HexaObserver hexaObserver = new HexaObserver(subject);
        OctalObserver octalObserver = new OctalObserver(subject);
        BinaryObserver binaryObserver = new BinaryObserver(subject);
-
+ub sesajl
        System.out.println("First state change: 15");
        subject.setState(15);
        
-       octalObserver.update();
+       octalObserver.update();jgjjhjhjgit
        
        System.out.println("\nAfter deactivating OctalObserver:");
        System.out.println("Second state change: 10");
